df6e0ad2fc9d725328ceeed662dee52b1574c48d
jomue <jomue@DESKTOP-1B4OUSJ>
2023-07-30 12:43:04 -0400

    Joan

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -1,8 +1,13 @@
 package demo;
 
 public class DemoDay3 {
-public static void main(String[] args) {
- System.out.println("Yasir");
+	public static void main(String[] args) {
+		System.out.println("Yasir");
 //importance of following stash process && communicating... :)
-}
+
+		System.out.println("Demo..");
+
+		System.out.println("Joan...");
+	}
+
 }
