d9b8fd02fe1c6aa7c294b2427ddab5b79ef7c063
YalDarraji <140679762+YalDarraji@users.noreply.github.com>
2023-07-30 12:41:03 -0400

    Update DemoDay3.java

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -1,6 +1,8 @@
 package demo;
 
 public class DemoDay3 {
-
+public static void main(String[] args) {
+ System.out.println("Yasir");
 //importance of following stash process && communicating... :)
+}
 }
